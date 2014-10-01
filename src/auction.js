21599d904bda71dc75f478f0218d551a00578be0
Gary Johnson <jazzbassjohnson@gmail.com>
2014-10-01 11:16:11 -0700

    feat(auction.js) newEntry stores each new entry in an array on the constructor

--- src/auction.js
+++ src/auction.js
@@ -3,13 +3,14 @@
 'use strict';
 
 function Auction() {
-  
+  this.$$entriesCollection = [];
 }
 
 Auction.prototype.$newEntry = function(nameOfItem,  startingPrice) {
     var entry = {};
     entry.nameOfItem = nameOfItem;
     entry.startingPrice = startingPrice;
+    this.$$entriesCollection.push(entry);
 
     return entry;
 };
