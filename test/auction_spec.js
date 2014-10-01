42222f324ce48d106a98c7c054613bdb0fa5c633
Gary Johnson <jazzbassjohnson@gmail.com>
2014-10-01 11:35:42 -0700

    test(auction_spec) BidderConstructor returns a bidder instance that can hold properties like and object

--- test/auction_spec.js
+++ test/auction_spec.js
@@ -6,28 +6,29 @@
 describe("Auction", function() {
 
   it("can be constructed and used as an object", function() {
-    var auction = new Auction();
-    auction.aProperty = 1;
+    var auctionInstance = new Auction();
+    auctionInstance.aProperty = 1;
 
-    expect(auction.aProperty).toBe(1);
+    expect(auctionInstance.aProperty).toBe(1);
   });
+
   describe("$newEntry", function() {
-    var auction;
+    var auctionInstance;
     beforeEach(function() {
-      auction = new Auction();
+      auctionInstance = new Auction();
     });
 
     it("returns an object", function() {
-      var entry = auction.$newEntry();
+      var entry = auctionInstance.$newEntry();
 
-      expect(typeof entry).toBe('object');
+      expect(typeof entry).toBe("object");
     });
 
     it("take 3 parameters, startingPrice and nameOfItem, and populates an object with them", function() {
       var item = "Toaster";
       var price = 40;
 
-      var entry = auction.$newEntry( item, price );
+      var entry = auctionInstance.$newEntry( item, price );
 
       expect(entry.nameOfItem).toBe(item);
       expect(entry.startingPrice).toBe(price);
@@ -39,16 +40,30 @@ describe("Auction", function() {
       var item2 = "Blender";
       var item2_price = 100;
 
-      var entry1 = auction.$newEntry(item1, item1_price);
-      var entry2 = auction.$newEntry(item2, item2_price);
+      var entry1 = auctionInstance.$newEntry(item1, item1_price);
+      var entry2 = auctionInstance.$newEntry(item2, item2_price);
 
-      expect(auction.$$entriesCollection[0].nameOfItem).toBe(entry1.nameOfItem);
-      expect(auction.$$entriesCollection[0].startingPrice).toBe(entry1.startingPrice)
-      
-      expect(auction.$$entriesCollection[1].nameOfItem).toBe(entry2.nameOfItem);
-      expect(auction.$$entriesCollection[1].startingPrice).toBe(entry2.startingPrice)
+      expect(auctionInstance.$$entriesCollection[0].nameOfItem).toBe(entry1.nameOfItem);
+      expect(auctionInstance.$$entriesCollection[0].startingPrice).toBe(entry1.startingPrice)
+
+      expect(auctionInstance.$$entriesCollection[1].nameOfItem).toBe(entry2.nameOfItem);
+      expect(auctionInstance.$$entriesCollection[1].startingPrice).toBe(entry2.startingPrice)
     });
 
   });
+
+  describe("$BidderConstructor", function() {
+    var auctionInstance;
+    beforeEach(function() {
+      auctionInstance = new Auction();
+    });
+
+    it("returns a bidder instance that can hold properties like and object", function() {
+      var bidder = new auctionInstance.$BidderConstructor();
+      bidder.aValue = 'something';
+
+      expect(bidder.aValue).toBe('something');
+    });
+  })
   
 });
