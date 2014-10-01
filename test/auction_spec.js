828652cffb31b1fe26af21e5584eb3998d9daa84
Gary Johnson <jazzbassjohnson@gmail.com>
2014-10-01 11:14:45 -0700

    test(auction_spec) newEntry stores each new entry in an array on the constructor

--- test/auction_spec.js
+++ test/auction_spec.js
@@ -34,10 +34,19 @@ describe("Auction", function() {
     });
 
     it("stores each new entry in an array on the constructor", function() {
-      var entry1 = auction.$newEntry();
-      var entry2 = auction.$newEntry();
+      var item1 = "Toaster";
+      var item1_price = 40;
+      var item2 = "Blender";
+      var item2_price = 100;
 
-      expect(auction.$$entriesCollection).toBe([entry1, entry2]);
+      var entry1 = auction.$newEntry(item1, item1_price);
+      var entry2 = auction.$newEntry(item2, item2_price);
+
+      expect(auction.$$entriesCollection[0].nameOfItem).toBe(entry1.nameOfItem);
+      expect(auction.$$entriesCollection[0].startingPrice).toBe(entry1.startingPrice)
+      
+      expect(auction.$$entriesCollection[1].nameOfItem).toBe(entry2.nameOfItem);
+      expect(auction.$$entriesCollection[1].startingPrice).toBe(entry2.startingPrice)
     });
 
   });
