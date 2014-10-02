d4320fab5d4ceec57b5a9774a2d679ebb3806998
Gary Johnson <jazzbassjohnson@gmail.com>
2014-10-01 19:30:58 -0700

    feat(auction.js) add newBidder function to the constructor

--- src/auction.js
+++ src/auction.js
@@ -2,7 +2,28 @@
 
 'use strict';
 
-var AuctionConstructor = function() {
-
-    this.$$sellerPrice = null;
+var Auction = function(sellerPrice) {
+    this.$$sellerPrice = sellerPrice || null;
+    this.$$biddersCollection = [];
 };
+
+Auction.prototype.newBidder = function(startingBid, maximumBid, automaticIncrementAmount) {
+    if(typeof startingBid !== 'number' || typeof maximumBid !== 'number' || typeof automaticIncrementAmount !== 'number') {
+        return null;
+    }
+    var self = this;
+    var bidder = {
+        startingBid: startingBid,
+        maximumBid: maximumBid,
+        automaticIncrementAmount: automaticIncrementAmount,
+        bidder_id: self.serialize()
+    };
+
+    this.$$biddersCollection.push(bidder);
+    return bidder;
+};
+
+Auction.prototype.serialize = function(){
+    return this.$$biddersCollection.length;
+};
+
