6acc7c3b96d1d9061411610366965a03fece6f22
Gary Johnson <jazzbassjohnson@gmail.com>
2014-10-01 18:31:42 -0700

    test(auction_spec) AuctionConstructor gives each instance a property called 59105sellerPrice

--- test/auction_spec.js
+++ test/auction_spec.js
@@ -8,8 +8,13 @@ describe("AuctionConstructor", function() {
   it("can be constructed and used as an object", function() {
     var Auction = new AuctionConstructor();
     Auction.aProperty = 1;
-
     expect(Auction.aProperty).toBe(1);
   });
+
+  it("gives each instance a property called $$sellerPrice", function() {
+    var Auction = new AuctionConstructor();
+    expect(Auction.hasOwnProperty('$$sellerPrice')).toBe(true);
+  });
+
   
 });
