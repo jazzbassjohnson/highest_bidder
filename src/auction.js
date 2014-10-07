33d7b362b5ad644a385dff90433d43cc3ff9be2e
Gary Johnson <jazzbassjohnson@gmail.com>
2014-10-06 18:02:01 -0700

    refactor(*) substancially change the functionality of the Auction constructor

--- src/auction.js
+++ src/auction.js
@@ -3,53 +3,57 @@
 'use strict';
 
 var Auction = function(sellerPrice) {
-    this.$$sellerPrice = sellerPrice || null;
-    this.$$biddersCollection = [];
+    this.$$biddersCollection = {};
+    this.$$bidderCount = 0;
 };
 
 Auction.prototype.newBidder = function(startingBid, maximumBid, automaticIncrementAmount) {
+    // check for valid input parameters
     if(typeof startingBid !== 'number' || typeof maximumBid !== 'number' || typeof automaticIncrementAmount !== 'number') {
         throw "Error: User data invalid";
     }
 
-    if(maximumBid < this.$$sellerPrice) {
-      throw "Bidder's maximum bid is too low to be considered";
-    }
-    
-    var self = this;
-    
     var BidderConstructor = function() {};
     BidderConstructor.prototype = this;
     
     var bidder = new BidderConstructor();
     bidder.startingBid = startingBid;
-    bidder.maximumBid = maximumBid;
+    bidder.maximumBid = this.adjustMaximumBid(startingBid, maximumBid, automaticIncrementAmount);
     bidder.automaticIncrementAmount = automaticIncrementAmount;
-    bidder.bidder_id = self.serialize();
-    bidder.auctionRoom = this;
+    bidder.bidder_id = this.serialize();
 
-    this.$$biddersCollection.push(bidder);
+    // tie breaker
+    if(!this.$$biddersCollection[bidder.maximumBid]) {
+      this.$$biddersCollection[bidder.maximumBid] = bidder;
+    }
+
     return bidder;
 };
 
-Auction.prototype.removeBid = function() {
-    var siblings = this.auctionRoom.$$biddersCollection;
-  var indexOfThis = siblings.indexOf(this);
-  if(indexOfThis >= 0) {
-    siblings.splice(indexOfThis, 1);
-  }
+// adjusts the maximum bid for each bidder based on their specifications
+Auction.prototype.adjustMaximumBid = function(startingBid, maximumBid, automaticIncrementAmount) {
+  return maximumBid - ((maximumBid - startingBid ) % automaticIncrementAmount);
 };
 
+
 Auction.prototype.serialize = function(){
-    return this.$$biddersCollection.length;
+    var id = this.$$bidderCount;
+    this.$$bidderCount++;
+    return id;
 };
 
 Auction.prototype.placeBids = function() {
-    var biddersCollection = this.$$biddersCollection;
+    // convert object to an array
+    var biddersCollection = [];
+    for(var key in this.$$biddersCollection) {
+      biddersCollection.push(this.$$biddersCollection[key]);
+    }
+
     var collectionLength = biddersCollection.length;
     if(!collectionLength) {
         throw "There are no bidders for this entry... :/";
     }
+
     if(collectionLength === 1) {
         return {
             winning_bid: this.$$sellerPrice,
@@ -57,50 +61,48 @@ Auction.prototype.placeBids = function() {
         };
     }
 
+
     var highestBidder = this.$findHighestBidder(biddersCollection);
 
-    var secondHighestBid = this.$findSecondHighestBidder(biddersCollection, highestBidder.maximumBid);
+    var secondHighestBidder = this.$findSecondHighestBidder(biddersCollection, highestBidder.maximumBid);
 
     var winner = {};
     winner.bidder_id = highestBidder.bidder_id;
-    winner.winning_bid = this.$findWinner(secondHighestBid, highestBidder.automaticIncrementAmount, highestBidder.startingBid);
+    winner.winning_bid = this.$findWinner(secondHighestBidder, highestBidder);
   
     return winner;
 };
 
 Auction.prototype.$findHighestBidder = function(biddersCollection) {
-  var currentHighestBidder = null;
-  _.forEach(biddersCollection, function( bidder, i ) {
-    if((currentHighestBidder === null) || (bidder.maximumBid > currentHighestBidder.maximumBid)) {
-      currentHighestBidder = bidder;
-    }
-  });
+  var sortedCollection = biddersCollection.sort(function(a,b){return a.maximumBid - b.maximumBid;});
+  var currentHighestBidder = sortedCollection[sortedCollection.length - 1];
+
   return currentHighestBidder;
 };
 
 Auction.prototype.$findSecondHighestBidder = function(biddersCollection, highestBid) {
-  var secondMaxBid = null;
-  _.forEach(biddersCollection, function( bidder, i ) {
-    var bidderMax = bidder.maximumBid;
-    if((secondMaxBid === null) || (bidderMax > secondMaxBid.maximumBid && bidderMax !== highestBid)) {
-      secondMaxBid = bidder;
-    }
-  });
-  return secondMaxBid;
-};
-
-Auction.prototype.$findWinner = function(secondHighestBid, highestBidderAutomaticIncrementAmount, highestBidderStartingBid) {
+  var sortedCollection = biddersCollection.sort(function(a,b){return a.maximumBid - b.maximumBid;});
   
-  var winningBid = this.$adjustStartingBid(highestBidderStartingBid, highestBidderAutomaticIncrementAmount);
-  while(winningBid < secondHighestBid) {
-    winningBid += highestBidderAutomaticIncrementAmount;
+  for(var i = biddersCollection.length-1; i >= 0; i--) {
+    if(sortedCollection[i].maximumBid !== highestBid) {
+      return sortedCollection[i];
+    }
   }
-  return winningBid;
+  return null;
 };
 
-Auction.prototype.$adjustStartingBid = function( startingBid, automaticIncrementAmount ) {
-  while(startingBid < this.$$sellerPrice) {
-    startingBid += automaticIncrementAmount;
+Auction.prototype.$findWinner = function(secondHighestBidder, highestBidder) {
+  var result = highestBidder.maximumBid;
+  var secondMaxBid = secondHighestBidder.maximumBid;
+
+  var tie = highestBidder.bidder_id > secondHighestBidder.bidder_id;
+  var automaticIncrementAmount = highestBidder.automaticIncrementAmount;
+  
+  while((result - automaticIncrementAmount) > secondMaxBid) {
+    result -= highestBidder.automaticIncrementAmount;
   }
-  return startingBid;
+  if((result - automaticIncrementAmount === secondMaxBid) && tie)  {
+    result -= highestBidder.automaticIncrementAmount;
+  }
+  return result;
 };
